77130b39d96407e66eab607ee536065bedf61b0e
Zachary <zcy621799@163.com>
2020-10-20 19:42:18 +0800

    clean up

--- else/Integrator/Lighttracing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "device_launch_parameters.h"
-#include "../Sampling.h"
-#include <thrust/random.h>
-#include <cuda_runtime.h>
-#include "../Resource.h"
-
-__global__ void LightTracingInit();
-__global__ void LightTracing(int iter, int maxDepth);
--- else/Integrator/Path.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "device_launch_parameters.h"
-#include "../Sampling.h"
-#include <thrust/random.h>
-#include <cuda_runtime.h>
-#include "../Resource.h"
-
-
-__global__ void Path(int iter, int maxDepth);
