e3a97bc5c0142b97053f53408705d8fe7920bae8
Zachary <zcy621799@163.com>
2020-10-27 23:10:26 +0800

    static bindables

---
+++ CUDA-PathTracer/Editor/Drawable/DrawableBase.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Drawable.h"
+#include "../Bindable/IndexBuffer.h"
+
+template<class T>
+class DrawableBase : public Drawable
+{
+public:
+	bool IsStaticInitialized() const noexcept
+	{
+		return !staticBinds.empty();
+	}
+	void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
+	{
+		assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+		staticBinds.push_back(std::move(bind));
+	}
+	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept(!IS_DEBUG)
+	{
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		pIndexBuffer = ibuf.get();
+		staticBinds.push_back(std::move(ibuf));
+	}
+	void SetIndexFromStatic() noexcept(!IS_DEBUG)
+	{
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		for (const auto& b : staticBinds)
+		{
+			if (const auto p = dynamic_cast<IndexBuffer*>(b.get()))
+			{
+				pIndexBuffer = p;
+				return;
+			}
+		}
+		assert("Failed to find index buffer in static binds" && pIndexBuffer != nullptr);
+	}
+private:
+	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept override
+	{
+		return staticBinds;
+	}
+private:
+	static std::vector<std::unique_ptr<Bindable>> staticBinds;
+};
+
+template<class T>
+std::vector<std::unique_ptr<Bindable>> DrawableBase<T>::staticBinds;
