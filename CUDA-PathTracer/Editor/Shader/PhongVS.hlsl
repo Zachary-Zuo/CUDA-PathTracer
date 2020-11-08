193e2e75934b4f43d361671b80074212d60754d3
Zachary <zcy621799@163.com>
2020-11-08 14:00:47 +0800

    need to work in camera's frame of reference for specular code to work

--- CUDA-PathTracer/Editor/Shader/PhongVS.hlsl
+++ CUDA-PathTracer/Editor/Shader/PhongVS.hlsl
@@ -1,21 +1,21 @@
 cbuffer CBuf
 {
-	matrix model;
-	matrix modelViewProj;
+    matrix modelView;
+    matrix modelViewProj;
 };
 
 struct VSOut
 {
-	float3 worldPos : Position;
-	float3 normal : Normal;
-	float4 pos : SV_Position;
+    float3 worldPos : Position;
+    float3 normal : Normal;
+    float4 pos : SV_Position;
 };
 
-VSOut main( float3 pos : Position,float3 n : Normal )
+VSOut main(float3 pos : Position, float3 n : Normal)
 {
-	VSOut vso;
-	vso.worldPos = (float3)mul( float4(pos,1.0f),model );
-	vso.normal = mul( n,(float3x3)model );
-	vso.pos = mul( float4(pos,1.0f),modelViewProj );
-	return vso;
+    VSOut vso;
+    vso.worldPos = (float3) mul(float4(pos, 1.0f), modelView);
+    vso.normal = mul(n, (float3x3) modelView);
+    vso.pos = mul(float4(pos, 1.0f), modelViewProj);
+    return vso;
 }
--- CUDA-PathTracer/Editor/Shader/SolidVS.hlsl
+++ CUDA-PathTracer/Editor/Shader/SolidVS.hlsl
@@ -1,10 +1,10 @@
 cbuffer CBuf
 {
-	matrix model;
-	matrix modelViewProj;
+    matrix modelView;
+    matrix modelViewProj;
 };
 
-float4 main( float3 pos : Position ) : SV_Position
+float4 main(float3 pos : Position) : SV_Position
 {
-	return mul( float4(pos,1.0f),modelViewProj );
+    return mul(float4(pos, 1.0f), modelViewProj);
 }
