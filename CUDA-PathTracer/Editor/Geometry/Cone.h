4aea590701ab355b44204e9bcad8c512e7289ec5
Zachary <zcy621799@163.com>
2020-11-08 19:10:09 +0800

    update cone and pyramid

--- CUDA-PathTracer/Editor/Geometry/Cone.h
+++ CUDA-PathTracer/Editor/Geometry/Cone.h
@@ -3,6 +3,7 @@
 #include <DirectXMath.h>
 #include "../../CasterLabmath.h"
 
+
 class Cone
 {
 public:
@@ -13,7 +14,7 @@ public:
 		assert(longDiv >= 3);
 
 		const auto base = dx::XMVectorSet(1.0f, 0.0f, -1.0f, 0.0f);
-		const float longitudeAngle = 2.0f * 3.14159265f / longDiv;// PI / longDiv;
+		const float longitudeAngle = 2.0f * 3.14159265f / longDiv;
 
 		// base vertices
 		std::vector<V> vertices;
@@ -51,6 +52,69 @@ public:
 			indices.push_back(iLong);
 			indices.push_back((iLong + 1) % longDiv);
 			indices.push_back(iTip);
+		}
+
+		return { std::move(vertices),std::move(indices) };
+	}
+	template<class V>
+	static IndexedTriangleList<V> MakeTesselatedIndependentFaces(int longDiv)
+	{
+		namespace dx = DirectX;
+		assert(longDiv >= 3);
+
+		const auto base = dx::XMVectorSet(1.0f, 0.0f, -1.0f, 0.0f);
+		const float longitudeAngle = 2.0f * 3.14159265f / longDiv;
+
+		std::vector<V> vertices;
+
+		// cone vertices
+		const auto iCone = (unsigned short)vertices.size();
+		for (int iLong = 0; iLong < longDiv; iLong++)
+		{
+			const float thetas[] = {
+				longitudeAngle * iLong,
+				longitudeAngle * (((iLong + 1) == longDiv) ? 0 : (iLong + 1))
+			};
+			vertices.emplace_back();
+			vertices.back().pos = { 0.0f,0.0f,1.0f };
+			for (auto theta : thetas)
+			{
+				vertices.emplace_back();
+				const auto v = dx::XMVector3Transform(
+					base,
+					dx::XMMatrixRotationZ(theta)
+				);
+				dx::XMStoreFloat3(&vertices.back().pos, v);
+			}
+		}
+		// base vertices
+		const auto iBaseCenter = (unsigned short)vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = { 0.0f,0.0f,-1.0f };
+		const auto iBaseEdge = (unsigned short)vertices.size();
+		for (int iLong = 0; iLong < longDiv; iLong++)
+		{
+			vertices.emplace_back();
+			auto v = dx::XMVector3Transform(
+				base,
+				dx::XMMatrixRotationZ(longitudeAngle * iLong)
+			);
+			dx::XMStoreFloat3(&vertices.back().pos, v);
+		}
+
+		std::vector<unsigned short> indices;
+
+		// cone indices
+		for (unsigned short i = 0; i < longDiv * 3; i++)
+		{
+			indices.push_back(i + iCone);
+		}
+		// base indices
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			indices.push_back(iBaseCenter);
+			indices.push_back((iLong + 1) % longDiv + iBaseEdge);
+			indices.push_back(iLong + iBaseEdge);
 		}
 
 		return { std::move(vertices),std::move(indices) };
